d00c8e0ae4b22fb8fc0dd3636598708686598c81
Viktor <Viktor@DESKTOP-RI5BCCR>
2019-03-24 17:35:44 +0100

    Kan connecta en client till en server.

--- Quiet/src/Client.java
+++ Quiet/src/Client.java
@@ -1,4 +1,9 @@
+import java.io.IOException;
+import java.net.Socket;
+
 
 public class Client {
-
+	public static void main(String arg[]) throws IOException{
+		Socket socket = new Socket("Localhost", 4555);
+	}
 }
--- Quiet/src/Server.java
+++ Quiet/src/Server.java
@@ -1,4 +1,13 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
 
+	public static void main(String arg[]) throws IOException{
+		ServerSocket serverSocket = new ServerSocket(4555);
+		Socket socket = serverSocket.accept();
+		
+		System.out.println("Client connected");
+	}
 }
