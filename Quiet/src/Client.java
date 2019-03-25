c9263f0f5d447df3bd6a2f3f2ade198337e9f80b
Viktor <Viktor@DESKTOP-RI5BCCR>
2019-03-25 20:46:28 +0100

    Skickar en sträng från client till server

--- Quiet/src/Client.java
+++ Quiet/src/Client.java
@@ -1,9 +1,16 @@
+import java.io.BufferedOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 
 
 public class Client {
 	public static void main(String arg[]) throws IOException{
+		String str = "hej";
 		Socket socket = new Socket("Localhost", 4555);
+		try (ObjectOutputStream oos = new ObjectOutputStream(new BufferedOutputStream(socket.getOutputStream()))) {
+			oos.writeObject(str);
+		} catch (IOException e) {}
+				
 	}
 }
--- Quiet/src/Server.java
+++ Quiet/src/Server.java
@@ -1,13 +1,18 @@
+import java.io.BufferedInputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
 
-	public static void main(String arg[]) throws IOException{
+	public static void main(String arg[]) throws IOException, ClassNotFoundException{
 		ServerSocket serverSocket = new ServerSocket(4555);
 		Socket socket = serverSocket.accept();
-		
+		try (ObjectInputStream ois = new ObjectInputStream(new BufferedInputStream(socket.getInputStream()))) {
+			String str = (String)ois.readObject();
+			System.out.println(str);
+		} catch (IOException e) {}
 		System.out.println("Client connected");
 	}
 }
