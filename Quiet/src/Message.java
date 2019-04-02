bb438342dda5d794b3c53253d6cbb7a000bb43c3
vickeee97 <viktor.werngren97@gmail.com>
2019-04-02 11:03:15 +0200

    la till getKey() i message

--- Quiet/src/Message.java
+++ Quiet/src/Message.java
@@ -1,12 +1,18 @@
 
 public class Message {
 	private String text;
+	private String key;
 
-	public Message(String text) {
+	public Message(String text, String key) {
 		this.text = text;
+		this.key = key;
 	}
 
 	public String getText() {
 		return text;
+	}
+
+	public String getKey() {
+		return key;
 	}
 }
