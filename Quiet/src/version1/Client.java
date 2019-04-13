abbf892471e163bad5cc793fe09b6c3a22b2596a
vickeee97 <viktor.werngren97@gmail.com>
2019-04-13 14:57:21 +0200

    uppdaterade client klassen

--- Quiet/src/version1/Client.java
+++ Quiet/src/version1/Client.java
@@ -1,8 +1,10 @@
 package version1;
 import java.io.BufferedOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.LinkedList;
 
 /**
  * En klass som hanterar det som skickas fr�n klienten.
@@ -10,6 +12,34 @@ import java.net.Socket;
  *
  */
 public class Client {
+	private Socket socket;
+	private User user;
+	private LinkedList<Message> recievedMessages = new LinkedList<Message>();
+	private String ip;
+	private int port;
+	
+	private Client (String ip, int port) {
+		this.ip = ip;
+		this.port = port;
+	}
+	
+	public void run(){
+		try {
+			
+		}
+	}
+	
+	
+	public void connect(){
+		try{
+			socket = new Socket(ip,port);
+			ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+			ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+		} catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+	
 	public static void main(String arg[]) throws IOException{
 		String str = "hej";
 		
