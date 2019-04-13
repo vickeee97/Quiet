21326d3e87fbbf136ba73ac1ec4f9029985fb09b
vickeee97 <viktor.werngren97@gmail.com>
2019-04-13 14:45:19 +0200

    La till lite metoder och liknande som behövs i message klassen, samt gjorde en User klass som skapar ramverket för en User.

--- Quiet/src/version1/Message.java
+++ Quiet/src/version1/Message.java
@@ -1,4 +1,7 @@
 package version1;
+
+import java.util.LinkedList;
+
 /**
  * Klass som hanterar ramverket f�r ett message objekt.
  * @author Viktor
@@ -7,14 +10,41 @@ package version1;
 public class Message {
 	private String text;
 	private String key;
+	private User sender;
+	private LinkedList<User> recievers = new LinkedList<User>();
+	
 
-	public Message(String text, String key) {
+	public Message(String text, String key, User sender, LinkedList<User> recievers) {
 		this.text = text;
 		this.key = key;
+		this.sender = sender;
+		this.recievers = recievers;
 	}
-
+	
+	public Message (String text, User sender, LinkedList<User> recievers) {
+		this.text = text;
+		this.sender = sender;
+		this.recievers = recievers;
+	}
+	
+	public void setText(String text){
+		this.text = text;
+	}
+	
 	public String getText() {
-		return text;
+		return this.text;
+	}
+	
+	public User getSender() {
+		return sender;
+	}
+	
+	public void setReciever(LinkedList<User> recievers){
+		this.recievers = recievers;
+	}
+	
+	public LinkedList<User> getRecievers(){
+		return recievers;
 	}
 
 	public String getKey() {
