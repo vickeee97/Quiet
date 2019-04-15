76517915489e7c6cfe5932e4a90de34dadf06904
kajsapihl <kajsapihl@192.168.1.19>
2019-04-15 10:39:33 +0200

    Ändrade textarea

--- Quiet/src/version1/UIclient.java
+++ Quiet/src/version1/UIclient.java
@@ -8,12 +8,14 @@ import javax.swing.JFrame;
 import javax.swing.JRadioButton;
 import javax.swing.AbstractButton;
 import javax.swing.BorderFactory;
+import javax.swing.DefaultListModel;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JTextField;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 import javax.swing.JLabel;
 import javax.swing.JTextArea;
@@ -23,18 +25,21 @@ import java.awt.Color;
 import java.awt.Component;
 
 import javax.swing.JTextPane;
+import javax.swing.ListSelectionModel;
 /**
  * En klass som hanterar UI fï¿½r klienten fï¿½r att till exempel skicka medddelandna
  * @author Viktor
  *
  */
-public class UIclient extends JPanel implements ActionListener{
+public class UIclient extends JPanel implements ActionListener {
 
 	private Client client;
 	JFrame frame;
 	private AbstractButton textArea;
-	JTextArea textArea_2;
-	JTextArea textArea_1;
+	private JTextArea textArea_2;
+	private DefaultListModel<String> LMMessageList2;
+	private JButton btnNewButton_1 = new JButton("Encrypt and send");
+	
 
 	/**
 	 * Launch the application.
@@ -78,7 +83,7 @@ public class UIclient extends JPanel implements ActionListener{
 		btnNewButton.setBounds(555, 375, 109, 60);
 		frame.getContentPane().add(btnNewButton);
 
-		JButton btnNewButton_1 = new JButton("Encrypt and \n Send");
+		
 		btnNewButton_1.setBounds(674, 375, 168, 60);
 		frame.getContentPane().add(btnNewButton_1);
 //		event e = new event();
@@ -88,23 +93,27 @@ public class UIclient extends JPanel implements ActionListener{
 		list.setBounds(130, 163, 1, 1);
 		frame.getContentPane().add(list);
 
-		JScrollBar scrollBar = new JScrollBar();
-		scrollBar.setBounds(852, 11, 17, 353);
-		frame.getContentPane().add(scrollBar);
+//		JScrollBar scrollBar = new JScrollBar();
+//		scrollBar.setBounds(852, 11, 17, 353);
+//		frame.getContentPane().add(scrollBar);
 
 		JLabel lblNamn = new JLabel("Viktor");
 		lblNamn.setBounds(120, 37, 46, 14);
 		frame.getContentPane().add(lblNamn);
 
-		JScrollBar scrollBar_1 = new JScrollBar();
-		scrollBar_1.setBounds(179, 117, 17, 318);
-		frame.getContentPane().add(scrollBar_1);
+//		JScrollBar scrollBar_1 = new JScrollBar();
+//		scrollBar_1.setBounds(179, 117, 17, 318);
+//		frame.getContentPane().add(scrollBar_1);
 
-		JTextPane txtpnVnner = new JTextPane();
-		txtpnVnner.setText(" Vänner");
-		txtpnVnner.setBounds(10, 117, 168, 318);
-		frame.getContentPane().add(txtpnVnner);
-		txtpnVnner.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY));
+		DefaultListModel LMMessageList = new DefaultListModel();
+		JList messageList = new JList(LMMessageList);
+		JScrollPane scrollMessageList = new JScrollPane(messageList);
+		JScrollBar messageScrollBar = scrollMessageList.getVerticalScrollBar();
+//		JTextPane txtpnVnner = new JTextPane();
+//		txtpnVnner.setText(" Vï¿½nner");
+		scrollMessageList.setBounds(10, 117, 178, 318);
+		frame.getContentPane().add(scrollMessageList);
+//		scrollMessageList.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY));
 
 		JLabel label = new JLabel();
 		ImageIcon img = new ImageIcon(this.getClass().getResource("/insertImageHere.png"));
@@ -117,20 +126,31 @@ public class UIclient extends JPanel implements ActionListener{
 		frame.getContentPane().add(textArea_2);
 		textArea_2.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY));
 		
-		textArea_1 = new JTextArea();
-		textArea_1.setBounds(206, 11, 636, 353);
-		frame.getContentPane().add(textArea_1);
-		textArea_1.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY));
+//		textArea_1 = new JTextArea();
+		DefaultListModel LMMessageList2 = new DefaultListModel();
+		JList messageList2 = new JList(LMMessageList2);
+		JScrollPane scrollMessageList2 = new JScrollPane(messageList2);
+		JScrollBar messageScrollBar2 = scrollMessageList2.getVerticalScrollBar();
+		messageList2.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+		messageList2.setLayoutOrientation(JList.VERTICAL);
+		messageList2.setVisibleRowCount(-1);
+		scrollMessageList2.setBounds(206, 11, 636, 353);
+		frame.getContentPane().add(scrollMessageList2);
+
+		
+//		textArea_1.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY));
 
 	}
 		public void actionPerformed(ActionEvent e) {
-			String text = textArea_2.getText();
-			if(text == null ? "" == null : text.equals("")) {
-				textArea_1.setText("Please enter some text");
-			} else {
-				textArea_1.setText(text);
-			}
-			textArea_2.setText("");
-		}
+			if(e.getSource() == btnNewButton_1) {
+//			String text = textArea_2.getText();
+//			if(text == null ? "" == null : text.equals("")) {
+//				textArea_2.setText("Please enter some text");
+//			} else {
+//				LMMessageList2.addElement("hej");
+//			}
+//			textArea_2.setText("");
+				LMMessageList2.addElement("hej");
+		}}
 	}
 
