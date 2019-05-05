1ec1f45063a160824aacd8b3216f812afaa89937
Jacob Lundin <jacoblundin@live.com>
2019-05-05 17:54:43 +0200

    korrigerade koden så att den stämmer överens med riktlinjerna för kod

--- Quiet/src/Version6Emil/GUIClient.java
+++ Quiet/src/Version6Emil/GUIClient.java
@@ -22,7 +22,7 @@ import javax.swing.JTextArea;
 import javax.swing.ListSelectionModel;
 
 public class GUIClient extends JPanel implements ActionListener {
-	
+
 	private JPanel pnl = new JPanel(new BorderLayout());
 	private JPanel pnlWest = new JPanel(new GridLayout(2,1));
 	private JPanel pnlCenter= new JPanel(new BorderLayout());
@@ -31,10 +31,8 @@ public class GUIClient extends JPanel implements ActionListener {
 	private JTextArea taMessage= new JTextArea();
 	private JButton btnEcryptnSend = new JButton("Encrypt and send");
 	private JButton btnDecrypt = new JButton("Decrypt");
-	
 	private DefaultListModel<String> LMKontactList= new DefaultListModel<String>();
 	private JList<String> kontactList = new JList<String>(LMKontactList);
-	
 	private JScrollPane jspKontaktList = new JScrollPane(kontactList);
 	private DefaultListModel LMMessageList = new DefaultListModel();
 	private JList messageList = new JList(LMMessageList);
@@ -42,52 +40,50 @@ public class GUIClient extends JPanel implements ActionListener {
 	private ImageIcon img = new ImageIcon(this.getClass().getResource("/quiet.png"));
 	private JLabel lblBild = new JLabel(img);
 	private ClientController controller;
-	
 	private JButton keyBtn = new JButton("Get keys");
-	
+
 	public GUIClient() {
 		JScrollBar kontaktScrollBar = jspKontaktList.getVerticalScrollBar();
 		JScrollBar messageScrollBar = scrollMessageList.getVerticalScrollBar();
-	   
+
 		add(pnl);
 		pnlWest.setPreferredSize(new Dimension(200,400));
 		pnlCenter.setPreferredSize(new Dimension(400,400));
-		
+
 		pnlWest.setBackground(Color.WHITE);
 		pnlCenter.setBackground(Color.WHITE);
-		
+
 		pnl.add(pnlWest, BorderLayout.WEST);
-	    pnl.add(pnlCenter, BorderLayout.CENTER);
-	    
-	    jspKontaktList.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY));
-	    kontactList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+		pnl.add(pnlCenter, BorderLayout.CENTER);
+
+		jspKontaktList.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY));
+		kontactList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
 		kontactList.setLayoutOrientation(JList.VERTICAL);
 		kontactList.setVisibleRowCount(-1);
-	    pnlWest.add(lblBild);
+		pnlWest.add(lblBild);
 		pnlWest.add(jspKontaktList);
-	   
-	  
-	    pnlCenterS.add(btnDecrypt);
-	    pnlCenterS.add(btnEcryptnSend);
-	    pnlCenterS.add(keyBtn);
-	    
-	    pnlCenterSouth.add(pnlCenterS, BorderLayout.EAST);
-	    pnlCenterSouth.add(taMessage, BorderLayout.CENTER);
-	    
-	    pnlCenter.add(pnlCenterSouth, BorderLayout.SOUTH);
-	    pnlCenter.add(scrollMessageList, BorderLayout.CENTER);
-	    scrollMessageList.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY));
-	    
-	    messageList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+
+		pnlCenterS.add(btnDecrypt);
+		pnlCenterS.add(btnEcryptnSend);
+		pnlCenterS.add(keyBtn);
+
+		pnlCenterSouth.add(pnlCenterS, BorderLayout.EAST);
+		pnlCenterSouth.add(taMessage, BorderLayout.CENTER);
+
+		pnlCenter.add(pnlCenterSouth, BorderLayout.SOUTH);
+		pnlCenter.add(scrollMessageList, BorderLayout.CENTER);
+		scrollMessageList.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY));
+
+		messageList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
 		messageList.setLayoutOrientation(JList.VERTICAL);
 		messageList.setVisibleRowCount(-1);
-	    
-	    taMessage.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY));
-		
-	    btnDecrypt.addActionListener(this);
-	    btnEcryptnSend.addActionListener(this);
-	    
+
+		taMessage.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY));
+
+		btnDecrypt.addActionListener(this);
+		btnEcryptnSend.addActionListener(this);
 	}
+
 	public void setUserList(LinkedList<User> userList) {
 		if (LMKontactList.getSize() > 0) {
 			LMKontactList.removeAllElements();
@@ -98,39 +94,40 @@ public class GUIClient extends JPanel implements ActionListener {
 			LMKontactList.addElement(temp);
 		}
 	}
+
 	public List<String> getSelectedUsers() {
 		List<String> list=kontactList.getSelectedValuesList();
 		return list;
 	}
+
 	public String getMessage() {
 		return taMessage.getText();
 	}
+
 	public void setTextArea(String s) {
 		System.out.println("12");
 		LMMessageList.addElement(s);
 	}
-	
+
 	public void setController(ClientController controller) {
-    	this.controller=controller;
-    }
-	
+		this.controller=controller;
+	}
+
 	public void actionPerformed(ActionEvent e) {
 		if(e.getSource()==btnEcryptnSend) {
 			if (controller.checkClientKeys() == true) {
-			try {
-				controller.sendMessage();
-			} catch (Exception e1) {
-				
-				e1.printStackTrace();
+				try {
+					controller.sendMessage();
+				} catch (Exception e1) {
+					e1.printStackTrace();
+				}
 			}
-		}} if (e.getSource() == keyBtn) {
+		} if (e.getSource() == keyBtn) {
 			controller.getKeys();
 		} if (e.getSource() == btnDecrypt) {
 			controller.deCryptMsg(taMessage.getText());
 			String text = controller.getDecrypted();
 			JOptionPane.showMessageDialog(null, text, "Test", 0);
-			
 		}
 	}
-
 }
