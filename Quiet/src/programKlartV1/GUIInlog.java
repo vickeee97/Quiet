ebb815bb27c0e33b1d49d11ce647430b2c529f53
Emil Dahl <Emil Dahl@192.168.0.5>
2019-05-08 13:22:20 +0200

    Ändrade så att man inte kan skriva mellanslag i användarnamn.

--- Quiet/src/programKlartV1/GUIInlog.java
+++ Quiet/src/programKlartV1/GUIInlog.java
@@ -20,7 +20,7 @@ public class GUIInlog extends JPanel implements ActionListener {
 	
 	private JTextField tfUsername= new JTextField();
 //	private JTextField tfPassword= new JTextField();
-	private JLabel lblUsername = new JLabel("Användarnamn");
+	private JLabel lblUsername = new JLabel("Username");
 //	private JLabel lblPassword = new JLabel("Lösenord");
 //	private JButton btnLogin = new JButton("Logga in");
 	private JButton btnRegister = new JButton("Go online");
@@ -79,20 +79,22 @@ public class GUIInlog extends JPanel implements ActionListener {
 //			}
 			
 		 if(e.getSource()==btnRegister) {
-			if (tfUsername.getText().length() == 0) {
-				JOptionPane.showMessageDialog(null, "Det valda användarnamnet är för kort. Försök igen.");
-			}else {
-				JFrame frame = new JFrame("Quiet:" + tfUsername.getText());
-				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				frame.add(this.controller.getClient());
-				frame.pack();
-				frame.setVisible(true);
-				frame.setResizable(false);
-				setVisible(false);
-				controller.createUser(tfUsername.getText());
+			 String name = tfUsername.getText();
+			 if (!(name.isEmpty()) && !(Character.isWhitespace(name.charAt(0)))) {
+				 JFrame frame = new JFrame("Quiet:" + tfUsername.getText());
+					frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+					frame.add(this.controller.getClient());
+					frame.pack();
+					frame.setVisible(true);
+					frame.setResizable(false);
+					setVisible(false);
+					controller.createUser(tfUsername.getText());  } 
+				else { JOptionPane.showMessageDialog(null, "Username is not valid.", "fel", 0);
+				
 			}
 		}else if(e.getSource()==btnExit) {
 			System.exit(0);
 		}
 	}
 }
+
